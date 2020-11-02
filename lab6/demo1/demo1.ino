7ba33577354210d6429d0b0a0fb48f597c442ddd
zeze-zeze <zezectf@gmail.com>
2020-11-02 13:02:12 +0800

    labs

---
+++ lab6/demo1/demo1.ino
@@ -0,0 +1,30 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    int degree = Serial.parseInt();
+    if (Serial.read() == '\n') {
+      myservo.write(degree);
+      delay(15); 
+    }
+  }
+}
